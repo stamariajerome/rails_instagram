e0de301ee6ab6e2aef5c2fa99f8a8d3052e7300f
Jerome Sta. Maria <stamaria.jerome@gmail.com>
2016-09-29 22:21:00 +0800

    refactoring login_form

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -3,4 +3,8 @@ class Collection < ApplicationRecord
   validates :title, :presence => true
   validates :url, :presence => true
   validates :description, :presence => true, :length => { :maximum => 100 }
+
+  def to_param
+    "#{title}".parameterize
+  end
 end
